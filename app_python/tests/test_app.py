931fa0fc05cf07956af6a99cb8bf7b0ad7c0df5d
Asqar Arslanov <asqarslanov@gmail.com>
2026-02-07 10:49:16 +0300

    extract http

--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -1,9 +1,15 @@
+import httpx
 from fastapi.testclient import TestClient
 
 from app import app
 
 
-def test_get_index() -> None:
+def http_get(route: str) -> httpx.Response:
     with TestClient(app) as client:
-        response = client.get("/")
-        assert response.status_code == 200
+        response = client.get(route)
+        return response
+
+
+def test_get_index() -> None:
+    response = http_get("/")
+    assert response.status_code == 200
